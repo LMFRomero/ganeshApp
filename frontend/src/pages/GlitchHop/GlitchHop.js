ba4a2a04a620ca9874d3af093d5ad8bdfd1907f2
marcuscastelo <viniciuscastelo6@hotmail.com>
2020-03-07 23:51:25 -0300

    Fix GlitchHop background and responsiveness

--- frontend/src/pages/GlitchHop/GlitchHop.js
+++ frontend/src/pages/GlitchHop/GlitchHop.js
@@ -6,14 +6,23 @@ import './GlitchHop.css';
 
 import ClipWelcome from '../../assets/clip_welcome.mp4';
 
+let styleBKP;
+function setBackgroundBlack() {
+    styleBKP = document.body.style;
+    document.body.style = 'background: black !important;';
+}
 
-export default function GlitchHop() {
-    
-    let history = useHistory();
+function restoreBackground() {
+    document.body.style = styleBKP;
+}
+
+
+export default function GlitchHop(props) {
+    setBackgroundBlack();
 
     return (
         <React.Fragment>
-            <video autoPlay id="myVideo" onEnded={()=>history.push('/')}>
+            <video className="embed-responsive embed-responsive-16by9" autoPlay id="myVideo" onEnded={()=>{restoreBackground(); props.onEnded();}}>
                 <source src={ClipWelcome} type="video/mp4" />
             </video>
         </React.Fragment>
