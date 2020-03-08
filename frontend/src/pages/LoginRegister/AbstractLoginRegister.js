0dbeb19a8b58b43b3163f8d9cce5a7fb15c9d6f7
marcuscastelo <viniciuscastelo6@hotmail.com>
2020-03-08 11:25:56 -0300

    Fixing global CSS issues (heigth,bg)

--- frontend/src/pages/LoginRegister/AbstractLoginRegister.js
+++ frontend/src/pages/LoginRegister/AbstractLoginRegister.js
@@ -7,7 +7,7 @@ import './AbstractLoginRegister.css';
 
 export default function AbstractLoginRegister(props) {
     return (
-        <div className="row">
+        <div className="row alr-page">
             <div className="col-9 col-sm-6 col-md-5 col-lg-4 col-xl-3 mx-auto mt-5 alr-panel"> 
                 <div className="text-center mt-2">
                     <img className="img login-logo" alt="avatar" src={Avatar}/>
