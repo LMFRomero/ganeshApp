92be923b4552603a6b3557f1f8934fae166c36cf
marcuscastelo <viniciuscastelo6@hotmail.com>
2020-03-07 23:59:26 -0300

    Move routes from App.js to routes.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,8 @@
 import React from 'react';
-import './App.css';
+import Routes from './routes';
 
-import {BrowserRouter as Router, Route, Redirect} from 'react-router-dom';
-import GlitchHop from './pages/GlitchHop/GlitchHop';
-import LoginPage from './pages/LoginRegister/pages/LoginPage';
-import RegisterPage from './pages/LoginRegister/pages/RegisterPage';
-import { currentSession } from './services/session-manager';
-import RootRedirect from './components/RootRedirect';
-
-
-function App() {
+export default function App() {
   return (
-    <Router>
-      <Route path="/" exact component={RootRedirect}/>
-
-      <Route path="/login" exact component={LoginPage}/>
-      <Route path="/register" exact component={RegisterPage}/>
-      <Route path="/welcome" exact component={GlitchHop}/>
-      {/* TODO: 404 page */}
-    </Router>
+    <Routes/>
   );
 }
-
-export default App;
---
+++ frontend/src/routes.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import './App.css';
+
+import {BrowserRouter as Router, Route} from 'react-router-dom';
+import GlitchHop from './pages/GlitchHop/GlitchHop';
+import LoginPage from './pages/LoginRegister/pages/LoginPage';
+import RegisterPage from './pages/LoginRegister/pages/RegisterPage';
+import RootRedirect from './components/RootRedirect';
+
+
+export default function Routes() {
+  return (
+    <Router>
+      <Route path="/" exact component={RootRedirect}/>
+
+      <Route path="/login" exact component={LoginPage}/>
+      <Route path="/register" exact component={RegisterPage}/>
+      <Route path="/welcome" exact component={GlitchHop}/> {/* TODO: remove debug route */}
+      
+      {/* TODO: 404 page */}
+    </Router>
+  );
+}
