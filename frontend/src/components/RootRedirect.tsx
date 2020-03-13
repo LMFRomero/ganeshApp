e2fc359df1e7aa7ddae96ce8a8c452d812ca89ee
marcuscastelo <viniciuscastelo6@hotmail.com>
2020-03-13 14:04:11 -0300

    Fixing GlitchHop, again

--- frontend/src/components/RootRedirect.tsx
+++ frontend/src/components/RootRedirect.tsx
@@ -1,35 +1,61 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import { currentSession } from '../services/session-manager';
 import GlitchHop from '../pages/GlitchHop/GlitchHop';
 import Dashboard from '../pages/Dashboard/Dashboard';
 import { Redirect } from 'react-router-dom';
 
-export default function RootRedirect() {
-    const [finishedValidation, setFinishedValidation] = useState(false);
-    const [animationHasEnded, setAnimationHasEnded] = useState(false);
-    const [valid, setValid] = useState(false);
+type RootRedirectState = {
+    backendResponded: Boolean,
+    sessionValid: Boolean
+    animationEnded: Boolean
+}
 
-    async function validateSession() {
-        const isValid = await currentSession.isAuthenticated();
-        setValid(isValid); //Will change on next render
-        console.log(finishedValidation, animationHasEnded, isValid)
-        if (!isValid) currentSession.destroyCookie();
-        setFinishedValidation(true);
+export default class RootRedirect extends React.Component<{}, RootRedirectState>{
+    timeout: NodeJS.Timeout | null;
+    constructor() {
+        super({backendResponded:Boolean});
+        this.timeout = null;
+        this.validateSession();
+        this.setState({
+            backendResponded: false,
+            sessionValid: false,
+            animationEnded: false
+        });
+        this.validateSession.bind(this);
+        this.render.bind(this);
     }
 
-    useEffect(() => {
-        validateSession();
-        // eslint-disable-next-line
-    }, []);
+    async validateSession() {
+        const sessionValid = await currentSession.isAuthenticated();
+        if (!sessionValid) currentSession.destroyCookie();
+        this.setState({
+            sessionValid,
+            backendResponded: true
+        });
+        this.timeout = setTimeout(()=> {
+            this.setState({
+                animationEnded: true
+            })
+        }, 2000);
+    }
 
-    return (
-        <React.Fragment>
-        {
-            !finishedValidation ? 'Validating session...':
-            !valid ? <Redirect to="/login"/> :
-            !animationHasEnded ? <GlitchHop onEnded={()=>setAnimationHasEnded(true)}/> :
-            <Dashboard/>   
-        }
-        </React.Fragment>
-    )
+    componentWillUnmount() {
+        if (this.timeout)
+            clearTimeout(this.timeout);
+        this.timeout = null;
+    }
+
+    render() { 
+        return (
+            <React.Fragment>
+                {
+                    !this.state ? 'loading states' :
+                    !this.state.backendResponded ? 'Validating session...':
+                    !this.state.sessionValid ? <Redirect to="/login"/> :
+                    !this.state.animationEnded ? <GlitchHop/> :
+                    <Dashboard/>   
+                }
+            </React.Fragment>
+        )
+    }
 }
--- frontend/src/pages/GlitchHop/GlitchHop.tsx
+++ frontend/src/pages/GlitchHop/GlitchHop.tsx
@@ -5,31 +5,7 @@ import './GlitchHop.css';
 //@ts-ignore
 import ClipWelcome from '../../assets/clip_welcome.mp4';
 
-let styleBKP: CSSStyleDeclaration;
-function setBackgroundBlack() {
-    styleBKP = document.body.style;
-    document.body.style.setProperty('background', 'black','important');
-}
-
-function restoreBackground() {
-    //@ts-ignore
-    document.body.style = styleBKP;
-}
-
-let hasEnded = false;
-export default function GlitchHop(props: {onEnded: ()=>void}) {
-    function onEnded() {
-        if (hasEnded) return;
-        hasEnded = true;
-        restoreBackground();
-        if (props.onEnded)
-            props.onEnded();
-    }
-
-    setBackgroundBlack();
-    
-    setTimeout(onEnded, 2100);
-
+export default function GlitchHop() {
     return (
         <React.Fragment>
             <video className="embed-responsive embed-responsive-16by9" autoPlay muted={true} preload="none" id="myVideo">
