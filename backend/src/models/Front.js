aea5d59056ca09d6dd9478c55cb766b9782a04b0
Lucas <lucasmfromero@gmail.com>
2020-03-13 22:53:30 -0300

    Front model

---
+++ backend/src/models/Front.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+
+const frontSchema = new mongoose.Schema ({
+    name: String,
+    leader: String,
+    members: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User'
+    }],
+    meetings: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Meeting'
+    }]
+});
+
+module.exports = mongoose.model('Front', frontSchema);
