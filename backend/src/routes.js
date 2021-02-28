8d323f3459b96fd8a426989067ab641a66909954
Lucas <lucasmfromero@gmail.com>
2021-02-28 00:34:52 -0300

    Changed endpoints paths

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -15,44 +15,44 @@ const routes = express.Router();
 
 
 //register and login
-routes.post('/register', RequestUserController.store);
-routes.post('/validate', SessionController.isAuth, (req, res) => { res.status(200).end() });
-routes.post('/login', passport.authenticate('local'), (req, res) => res.status(200).end());
-routes.post('/logout', SessionController.isAuth, SessionController.destroy);
+routes.post('/api/register', RequestUserController.store);
+routes.post('/api/validate', SessionController.isAuth, (req, res) => { res.status(200).end() });
+routes.post('/api/login', passport.authenticate('local'), (req, res) => res.status(200).end());
+routes.post('/api/logout', SessionController.isAuth, SessionController.destroy);
 
 //new users management
-routes.post('/acceptUser', SessionController.isAuth, privilegeMan.canManageMembers, RequestUserController.update);
-routes.post('/rejectUser', SessionController.isAuth, privilegeMan.canManageMembers, RequestUserController.destroy);
-routes.post('/acceptUser/dev', SessionController.isAuth, RequestUserController.update);
-routes.post('/rejectUser/dev', SessionController.isAuth, RequestUserController.destroy);
+routes.post('/api/acceptUser', SessionController.isAuth, privilegeMan.canManageMembers, RequestUserController.update);
+routes.post('/api/rejectUser', SessionController.isAuth, privilegeMan.canManageMembers, RequestUserController.destroy);
+routes.post('/api/acceptUser/dev', SessionController.isAuth, RequestUserController.update);
+routes.post('/api/rejectUser/dev', SessionController.isAuth, RequestUserController.destroy);
 
 //reset password
-routes.post('/forgotPassword', ResetPasswordController.store);
-routes.post('/resetPassword/:token', ResetPasswordController.update);
+routes.post('/api/forgotPassword', ResetPasswordController.store);
+routes.post('/api/resetPassword/:token', ResetPasswordController.update);
 
 //promote and demote members
-routes.post('/promote/:username/dev', SessionController.isAuth, privilegeMan.changeRole);
-routes.post('/promote/:username', SessionController.isAuth, privilegeMan.canChangeRole, privilegeMan.changeRole);
+routes.post('/api/promote/:username/dev', SessionController.isAuth, privilegeMan.changeRole);
+routes.post('/api/promote/:username', SessionController.isAuth, privilegeMan.canChangeRole, privilegeMan.changeRole);
 
 //meetings
-routes.post('/meeting', SessionController.isAuth, MeetingController.store);
-routes.put('/meeting/:id', SessionController.isAuth, MeetingController.update);
-routes.delete('/meeting/:id', SessionController.isAuth, MeetingController.destroy);
-routes.get('/meeting', SessionController.isAuth, MeetingController.show);
-routes.post('/meeting/generate/:id', SessionController.isAuth, MeetingController.generateMeetingCode)
-routes.post('/meeting/:code', SessionController.isAuth, MeetingController.checkTime, MeetingController.checkMemberFrequency);
+routes.post('/api/meeting', SessionController.isAuth, MeetingController.store);
+routes.put('/api/meeting/:id', SessionController.isAuth, MeetingController.update);
+routes.delete('/api/meeting/:id', SessionController.isAuth, MeetingController.destroy);
+routes.get('/api/meeting', SessionController.isAuth, MeetingController.show);
+routes.post('/api/meeting/generate/:id', SessionController.isAuth, MeetingController.generateMeetingCode)
+routes.post('/api/meeting/:code', SessionController.isAuth, MeetingController.checkTime, MeetingController.checkMemberFrequency);
 
 
 //fronts
-routes.post('/front', SessionController.isAuth, privilegeMan.canManageFront, FrontController.store);
-routes.put('/front/:frontName', SessionController.isAuth, privilegeMan.canManageFront, FrontController.update);
-routes.delete('/front/:frontName', SessionController.isAuth, privilegeMan.canManageFront, FrontController.destroy);
-routes.post('/front/addUser/:frontName', SessionController.isAuth, privilegeMan.isSelf, FrontController.addUser);
-routes.post('/front/addUser/:frontName/adm', SessionController.isAuth, privilegeMan.canManageMembers, FrontController.addUser);
-routes.post('/front/removeUser/:frontName', SessionController.isAuth, privilegeMan.isSelf, FrontController.removeUser);
-routes.post('/front/removeUser/:frontName/adm', SessionController.isAuth, privilegeMan.canManageMembers, FrontController.removeUser);
-routes.post('/front/addMeeting/:frontName', SessionController.isAuth, FrontController.addMeeting);
-routes.post('/front/removeMeeting/:frontName', SessionController.isAuth, privilegeMan.canManageFront, FrontController.removeMeeting);
+routes.post('/api/front', SessionController.isAuth, privilegeMan.canManageFront, FrontController.store);
+routes.put('/api/front/:frontName', SessionController.isAuth, privilegeMan.canManageFront, FrontController.update);
+routes.delete('/api/front/:frontName', SessionController.isAuth, privilegeMan.canManageFront, FrontController.destroy);
+routes.post('/api/front/addUser/:frontName', SessionController.isAuth, privilegeMan.isSelf, FrontController.addUser);
+routes.post('/api/front/addUser/:frontName/adm', SessionController.isAuth, privilegeMan.canManageMembers, FrontController.addUser);
+routes.post('/api/front/removeUser/:frontName', SessionController.isAuth, privilegeMan.isSelf, FrontController.removeUser);
+routes.post('/api/front/removeUser/:frontName/adm', SessionController.isAuth, privilegeMan.canManageMembers, FrontController.removeUser);
+routes.post('/api/front/addMeeting/:frontName', SessionController.isAuth, FrontController.addMeeting);
+routes.post('/api/front/removeMeeting/:frontName', SessionController.isAuth, privilegeMan.canManageFront, FrontController.removeMeeting);
 
 
 
