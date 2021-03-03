9971acac370671e6bca36151482df9a342d0af43
Van Loon <gabriel.vl.rojas@gmail.com>
2021-03-03 10:30:54 -0300

    Extract api ErrorHandler to external file

--- frontend/src/services/userService.js
+++ frontend/src/services/userService.js
@@ -1,4 +1,5 @@
 import api from './_api'
+import handleErrors from './_handlers'
 import { authService } from './authService'
 
 export const userService = { 
@@ -56,25 +57,4 @@ function deleteAccount(userId, email) {
         .then(() => { document.location.reload() })
     })
     .catch(handleErrors) 
-}
-
-function handleErrors(error) { 
-    // Status Code is 401 -> Unauthorized
-    if(error.response.status === 401){
-        authService.logout()
-        .then(() => { document.location.reload() })
-    }
-
-    // Status Code out of range 2xx
-    if(error.response) {
-        console.log(error.response)
-        try {
-            return Promise.reject(error.response.data)
-        } catch(e) { 
-            return Promise.reject({ message:'Ocorreu um no processamento da requisição!' })
-        }
-    }
-
-    // No response received OR error in request settings
-    return Promise.reject({ message:'Ocorreu um erro no envio!' })
 }
