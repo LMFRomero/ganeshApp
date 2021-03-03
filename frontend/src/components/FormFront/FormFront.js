e318d8f4af8fc3bf62c5d57272e7a01d35643ab8
Van Loon <gabriel.vl.rojas@gmail.com>
2021-03-03 14:34:45 -0300

    Add frontService.register & update with mockup returns

--- frontend/src/components/FormFront/FormFront.js
+++ frontend/src/components/FormFront/FormFront.js
@@ -1,35 +1,124 @@
-import { Grid, Button } from '@material-ui/core'
-import { TextField, Checkbox, FormControlLabel } from '@material-ui/core'
+import { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+import { Grid, Button, FormControl, Select, MenuItem, InputLabel, TextField } from '@material-ui/core'
+
+import { frontService } from '../../services/frontService'
 
 // Variants: "register" and "edit"
-function FormFront(props){
+function FormFront({ variant, formSuccess, setFormSuccess, formErrors, setFormErrors }){
+
+  let { frontId } = useParams()
+
+  const [submitDisabled, setSubmitDisabled] = useState(false)
+  const [formData, setFormData]             = useState({
+    id:   '',
+    name: '',
+    description: '',
+    slug: '',
+    type: 'study',
+    membersOnly: true,
+    deleted: false,
+  })
+
+  useEffect(() => { 
+    if(variant === "edit"){
+      frontService.getById(frontId)
+      .then(function(u) { setFormData(u) })
+      .catch(  function(e) { 
+        setFormErrors(e) 
+        setSubmitDisabled(true)
+      }) 
+    }
+  }, [frontId, variant, setFormErrors])
+
+
+  const handleChange = (e) => {
+    const {name, value} = e.target
+    setFormData({...formData, [name]: value})
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    
+    setFormErrors({})
+    setFormSuccess({})
+    // setSubmitDisabled(true)
+
+    if(!formData.slug.match(/^[a-zA-Z0-9-]{3,16}$/)){
+      setFormErrors({repeatPassword: "Slug inválido! Use apenas alfanuméricos e traços!"})
+      setSubmitDisabled(false)
+      return;
+    }
+    
+    if(variant === "register"){ 
+      frontService.register(formData)
+      .then(   function(s) { setFormSuccess(s) })
+      .catch(  function(e) { setFormErrors(e) })     
+      .finally(function( ) { setSubmitDisabled(false) })
+    
+    } else if(variant === "edit") { 
+      frontService.update(frontId, formData)
+      .then(   function(s) { setFormSuccess(s) })
+      .catch(  function(e) { setFormErrors(e) })     
+      .finally(function( ) { setSubmitDisabled(false) }) 
+    } 
+  }
+
+
   return(
-    <>
-      <Grid item component="form" className="FormSection" xs={12} md={8}>
-        <TextField variant="filled" fullWidth label="Nome da Frente" required/>
+    <Grid item xs={12} container spacing={3} justify="center" 
+      component="form" onSubmit={handleSubmit} style={{paddingLeft: 0, paddingRight: 0}}>
 
-        <TextField variant="filled" fullWidth label="Conteúdo" required
-        multiline rows={4}/>
+      <Grid item className="FormSection" xs={12} md={7}>
+        
+        <TextField variant="filled" fullWidth label="Nome da Frente" name="name" value={formData.name}
+          required inputProps={{maxLength:32}} error={!!formErrors.name} onChange={handleChange} />
 
-        <FormControlLabel label="Visível apenas para membros ativos." 
-        control={
-            <Checkbox color="primary" name="membersOnly" checked/>
-        }/>
+        <TextField variant="filled" fullWidth label="Descrição e Informações Adicionais" name="description" value={formData.description}
+          multiline rows={8} inputProps={{maxLength:512}} error={!!formErrors.description} onChange={handleChange} />
+      
+      </Grid>
 
-        { props.variant === "register" &&  
-        <Button variant="contained" size="large" fullWidth color="secondary">
-          <strong>Publicar Frente</strong>
+      <Grid item className="FormSection" xs={12} md={5}>
+
+        <TextField variant="filled" fullWidth label="URL Slug" name="slug" value={formData.slug}
+          required inputProps={{minLength:3, maxLength:16}} error={!!formErrors.slug} onChange={handleChange} />
+
+        <FormControl variant="filled" fullWidth error={formErrors.type}>
+          <InputLabel id="LabelType">Tipo de Frente *</InputLabel>
+          <Select labelId="LabelType" label="Tipo de Frente *" name="type" value={formData.type}
+            required onChange={handleChange}>
+              <MenuItem value="study">Frente de Estudos (Listável)</MenuItem>
+              <MenuItem value="special">Frente Especial (Listável)</MenuItem>
+              <MenuItem value="internal">Frente Interna (Não Listável)</MenuItem>
+          </Select>
+        </FormControl>
+
+        <FormControl variant="filled" fullWidth error={formErrors.membersOnly}>
+          <InputLabel id="LabelMembersOnly">Visibilidade *</InputLabel>
+          <Select labelId="LabelMembersOnly" label="Visibilidade *" name="membersOnly" value={formData.membersOnly}
+            required onChange={handleChange}>
+              <MenuItem value={false}>Todos os Usuários</MenuItem>
+              <MenuItem value={true}>Apenas Membros Ativos</MenuItem>
+          </Select>
+        </FormControl>
+
+        <FormControl variant="filled" fullWidth error={formErrors.deleted}>
+          <InputLabel id="LabelStatus">Status *</InputLabel>
+          <Select labelId="LabelStatus" label="Status *" name="deleted" value={formData.deleted}
+            required onChange={handleChange}>
+              <MenuItem value={false}>Frente Ativa</MenuItem>
+              <MenuItem value={true}>Frente Desativada</MenuItem>
+          </Select>
+        </FormControl>
+        
+        <Button variant="contained" size="large" fullWidth color="secondary"
+          type="submit" disabled={submitDisabled}>
+          <strong>{variant === "register" ? "Publicar Frente" : "Salvar Alterações"}</strong>
         </Button>
-        }
-
-        { props.variant === "edit" &&
-        <Button variant="contained" size="large" fullWidth color="secondary">
-          <strong>Salvar Alterações</strong>
-        </Button>
-        }
 
       </Grid>
-    </>   
+    </Grid>   
   )
 }
 
--- frontend/src/pages/DefaultForm/DefaultForm.js
+++ frontend/src/pages/DefaultForm/DefaultForm.js
@@ -1,41 +1,54 @@
+import { useState } from 'react'
 import { Box, Container, Grid, Typography } from '@material-ui/core'
 
+import SnackAlerts from '../../components/SnackAlerts/SnackAlerts'
 import FormAnnounce from '../../components/FormAnnounce/FormAnnounce'
 import FormMeeting  from '../../components/FormMeeting/FormMeeting'
 import FormFront    from '../../components/FormFront/FormFront' 
 
+
 // Variants: 'register-meeting', 'edit-meeting', 'r/e-announce', 'r/e-front',
 function DefaultForm(props){
     
-    const [ action, subject ] = props.variant.split('-')
+  const [action, subject] = props.variant.split('-')
+  const [formSuccess, setFormSuccess]       = useState({})
+  const [formErrors, setFormErrors]         = useState({})
 
-    const options = {
-        announce: { name: 'Comunicado', render: (v) => <FormAnnounce variant={v}/> },
-        meeting:  { name: 'Reunião',    render: (v) => <FormMeeting variant={v}/> },
-        front:    { name: 'Frente',     render: (v) => <FormFront variant={v}/> },
-    }
+  const options = {
+    announce: { name: 'Comunicado', Component: FormAnnounce },
+    meeting:  { name: 'Reunião',    Component: FormMeeting },
+    front:    { name: 'Frente',     Component: FormFront },
+  }
 
-    const renderTitle = () => {
-        return (
-            <Typography variant="h3">
-                { (action === 'edit') ? 'Editar ' : 'Criar '}
-                { options[subject].name }
-            </Typography>
-        )
-    }
+  const renderTitle = () => {
+    return (
+      <Typography variant="h3">
+        { (action === 'edit') ? 'Editar ' : 'Criar '}
+        { options[subject].name }
+      </Typography>
+    )
+  }
 
-    const renderForm = () => options[subject].render(action)
+  const renderForm = (Component, action) => { 
+    return (
+      <Component variant={action} formSuccess={formSuccess} setFormSuccess={setFormSuccess}
+        formErrors={formErrors} setFormErrors={setFormErrors} />
+    )
+  }
 
   return(
     <Box className="DefaultFormPage" flexGrow={1} component="main">
       <Container fixed>
           <Grid container spacing={3} justify="center">
 
-            <Grid item xs={12} md={12}>
+            <SnackAlerts formSuccess={formSuccess} setFormSuccess={setFormSuccess} 
+              formErrors={formErrors} setFormErrors={setFormErrors}/>
+              
+            <Grid item xs={12}>
                 { renderTitle() }
             </Grid>
             
-            { renderForm() }
+            { renderForm(options[subject].Component, action) }
             
           </Grid>
       </Container>
