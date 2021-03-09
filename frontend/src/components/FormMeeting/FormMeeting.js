77a69612b08f9815a637f359777c75b9c662e2fa
Van Loon <gabriel.vl.rojas@gmail.com>
2021-03-08 21:12:15 -0300

    Add meting.register & update Services

--- frontend/src/components/FormMeeting/FormMeeting.js
+++ frontend/src/components/FormMeeting/FormMeeting.js
@@ -1,57 +1,153 @@
+import { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
 import { Grid, Button } from '@material-ui/core'
 import { TextField, InputLabel, Select, MenuItem, FormControl, Checkbox, FormControlLabel } from '@material-ui/core'
 
+import { meetingService } from '../../services/meetingService'
+import { frontService } from '../../services/frontService'
+
 // Variants: "register" and "edit"
-function FormMeeting(props){
+function FormMeeting({ variant, formSuccess, setFormSuccess, formErrors, setFormErrors }){
+
+  let { meetingId } = useParams()
+
+  const [submitDisabled, setSubmitDisabled] = useState(false)
+  const [frontOptions,   setFrontOptions]   = useState([])
+  const [formData, setFormData]             = useState({
+    id:   '', 
+    title: '', 
+    content: '',
+    frontSlug: '',
+    date: '',
+    time: '',
+    duration: '',
+    place: '',
+    membersOnly: true,
+    // deleted: false,
+  })
+
+  useEffect(() => { 
+    frontService.getOptions()
+    .then( (s) => setFrontOptions(s))
+    .catch((e) => { 
+      setFormErrors(e) 
+      setSubmitDisabled(true)
+    })
+
+    if(variant === "edit"){
+      meetingService.getById(meetingId)
+      .then(function(m) { 
+        setFormData({ ...m, 
+          frontSlug: m.front.slug,
+          date: m.date.substr(0,10),
+          time: m.date.substr(11,5)
+        }) 
+      })
+      .catch(function(e) { 
+        setFormErrors(e) 
+        setSubmitDisabled(true)
+      }) 
+    }
+  }, [meetingId, variant, setFormErrors])
+
+  const handleChange = (e) => {
+    const {name, value} = e.target
+    setFormData({...formData, [name]: value})
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    
+    setFormErrors({})
+    setFormSuccess({})
+    setSubmitDisabled(true)
+
+    // Transforming Datetime to Date Object and remove unused data
+    let meeting = { ...formData, date: new Date(`${formData.date} ${formData.time}`) }
+    delete meeting.time
+    delete meeting.author
+    delete meeting.front
+
+    if ( isNaN(meeting.date.valueOf()) ) {
+      setFormErrors({date: "Data ou Hora inválida!"})
+      setSubmitDisabled(false)
+      return;
+    } else { meeting.date = meeting.date.toISOString() }
+    
+    if(variant === "register"){ 
+      meetingService.register(meeting)
+      .then(   function(s) { setFormSuccess(s) })
+      .catch(  function(e) { setFormErrors(e) })     
+      .finally(function( ) { setSubmitDisabled(false) })
+    
+    } else if(variant === "edit") { 
+      meetingService.update(meetingId, meeting)
+      .then(   function(s) { setFormSuccess(s) })
+      .catch(  function(e) { setFormErrors(e) })     
+      .finally(function( ) { setSubmitDisabled(false) }) 
+    } 
+  }
+
   return(
-    <>
-      <Grid className="FormSection" item xs={12} md={6}>
-        <TextField variant="filled" fullWidth label="Título" required/>
+    <Grid item xs={12} container spacing={3} justify="center" 
+      component="form" onSubmit={handleSubmit} style={{paddingLeft: 0, paddingRight: 0}}>
+
+      <Grid className="FormSection" item xs={12} md={7}>
+
+        <TextField variant="filled" fullWidth label="Título" name="title" value={formData.title}
+          required inputProps={{maxLength:64}} error={!!formErrors.title} onChange={handleChange} />
+
+        <TextField variant="filled" fullWidth label="Conteúdo e Informações Adicionais" name="content" value={formData.content}
+          multiline rows={12} inputProps={{maxLength:1024}} error={!!formErrors.content} onChange={handleChange} />
         
-        <TextField variant="filled" fullWidth label="Conteúdo" required
-          multiline rows={11}/>
       </Grid>
 
-      <Grid className="FormSection" item xs={12} md={6}>
-        <FormControl variant="filled" fullWidth>
-          <InputLabel id="LabelFront">Frente</InputLabel>
-          <Select labelId="LabelFront" label="Frente">
-            <MenuItem value="Cripto">Criptografia</MenuItem>
-            <MenuItem value="RevPwn">Reversa e Pwning</MenuItem>
-            <MenuItem value="Redes">Redes</MenuItem>
-            <MenuItem value="Hard">Hardware</MenuItem>
-            <MenuItem value="Web">Web</MenuItem>
-            <MenuItem value="Geral">Reunião Geral</MenuItem>
+      <Grid className="FormSection" item xs={12} md={5}>
+
+        <FormControl variant="filled" fullWidth error={formErrors.frontSlug}>
+          <InputLabel id="LabelFront">Frente *</InputLabel>
+          <Select labelId="LabelFront" label="Frente *" name="frontSlug" value={formData.frontSlug}
+            required onChange={handleChange}>
+              { frontOptions.map((f,i)=><MenuItem key={f.slug} value={f.slug}>{f.name}</MenuItem>) }
           </Select>
         </FormControl>
+        
+        <Grid container spacing={3}>
+          <Grid item xs={6} style={{paddingTop: 0, paddingBottom: 0 }}>
+          <TextField type="date" variant="filled" fullWidth label="Data" name="date" value={formData.date}
+            required inputProps={{maxLength:64}} error={!!formErrors.date} onChange={handleChange} 
+            InputLabelProps={{ shrink: true }} />
+          </Grid>
+          
+          <Grid item xs={6} style={{paddingTop: 0, paddingBottom: 0 }}>
+          <TextField type="time" variant="filled" fullWidth label="Horário" name="time" value={formData.time}
+            required inputProps={{maxLength:64}} error={!!formErrors.date} onChange={handleChange} 
+            InputLabelProps={{ shrink: true }}/>
+          </Grid>
+        </Grid>
 
-        <TextField variant="filled" fullWidth type="date"required
-          label="Data" InputLabelProps={{ shrink: true}}/>
+        <TextField variant="filled" fullWidth label="Duração" name="duration" value={formData.duration}
+          inputProps={{maxLength:64}} error={!!formErrors.duration} onChange={handleChange} />
+
+        <TextField variant="filled" fullWidth label="Local" name="place" value={formData.place}
+          inputProps={{maxLength:64}} error={!!formErrors.place} onChange={handleChange} />
         
-        <TextField variant="filled" fullWidth type="time" required
-          label="Horário" InputLabelProps={{ shrink: true}}/>
+        <FormControl variant="filled" fullWidth error={formErrors.membersOnly}>
+          <InputLabel id="LabelMembersOnly">Visibilidade *</InputLabel>
+          <Select labelId="LabelMembersOnly" label="Visibilidade *" name="membersOnly" value={formData.membersOnly}
+            required onChange={handleChange}>
+              <MenuItem value={false}>Todos os Usuários</MenuItem>
+              <MenuItem value={true}>Apenas Membros Ativos</MenuItem>
+          </Select>
+        </FormControl>
         
-        <TextField variant="filled" fullWidth label="Local" required/>
-        
-        <FormControlLabel label="Visível apenas para membros ativos." 
-          control={
-            <Checkbox color="primary" name="PingParticipant" checked/>
-        }/>
-        
-        { props.variant === "register" &&  
-        <Button variant="contained" size="large" fullWidth color="secondary">
-          <strong>Publicar reunião</strong>
+        <Button variant="contained" size="large" fullWidth color="secondary"
+          type="submit" disabled={submitDisabled}>
+          <strong>{variant === "register" ? "Publicar Reunião" : "Salvar Alterações"}</strong>
         </Button>
-        }
-
-        { props.variant === "edit" &&
-        <Button variant="contained" size="large" fullWidth color="secondary">
-          <strong>Salvar Alterações</strong>
-        </Button>
-        }
-
       </Grid>
-    </>
+
+    </Grid>
   )
 }
 
---
+++ frontend/src/services/meetingService.js
@@ -0,0 +1,93 @@
+import api from './_api'
+// import handleErrors from './_handlers'
+
+export const meetingService = { 
+    register,
+    update,
+    _delete,
+    getById,
+    getAll,
+    addMember,
+    removeMember,
+}
+
+function register(meeting) { 
+    return api.post('/meeting', meeting)
+    // .then((response) => Promise.resolve(response.data))
+    // .catch(handleErrors)
+    .catch(() => Promise.resolve('Return mockup Data'))
+    .then(() => Promise.resolve({message: "Reunião cadastrada com sucesso!" }) )
+}
+
+function update(meetingId, meeting) {
+    return api.put(`/meeting/${meetingId}`, meeting)
+    // .then((response) => Promise.resolve(response.data))
+    // .catch(handleErrors) 
+    .catch(() => Promise.resolve('Return mockup Data'))
+    .then(() => Promise.resolve({message: "Reunião atualizada com sucesso!" }) )
+ }
+
+function _delete() { 
+    // return api.delete('/user/' + userId)
+    // .then( () => {
+    //     authService.logout()
+    //     .then(() => { document.location.reload() })
+    // })
+    // .catch(handleErrors) 
+}
+
+function getById(meetingId) { 
+    return api.get('/meeting/' + meetingId)
+    // .then((response) => Promise.resolve(response.data))
+    // .catch(handleErrors)
+    .catch(() => Promise.resolve('Return mockup Data'))
+    .then(() => {
+        return Promise.resolve({
+            id:'M337', title: 'Reunião de Web - SQL Injection + HackTheBox', 
+            date: '2021-03-14T18:02:00.694Z', duration: '2 horas', place: 'Online (Canal #web Discord)',
+            content: 'Lorem Ipsum Dolor Sit Amet Veras Tamen',
+            front:  { name: 'Segurança Web', slug: 'seguranca-web' },
+            membersOnly: true, deleted: false,
+
+            members: [ {username:"teste"}, {username:"Gabriel"}, {username:"Kairi"}, {username:"Axel"}],
+
+            author: { username: "Gabriel Van Loon", title: "Membro" },
+            publishDate: '2021-03-14T18:02:00.694Z'
+        })
+    })
+}
+
+function getAll( page = 1, frontSlug = '') { 
+    return api.get(`/meetings/${page}/${frontSlug}`)
+    // .then((response) => Promise.resolve(response.data))
+    // .catch(handleErrors)
+    .catch(() => Promise.resolve('Return mockup Data'))
+    .then((response) => {
+        return Promise.resolve([
+            {
+                id:'M337', title: 'Reunião de Web - SQL Injection + HackTheBox', 
+                date: '2021-03-14T18:02:00.694Z', duration: '2 horas', place: 'Online (Canal #web Discord)',
+                front:  { name: 'Segurança Web', slug: 'seguranca-web' },
+                author: { username: "Gabriel Van Loon", title: "Membro" },
+                publishDate: '2 de fevereiro de 2020, 13h37',
+                membersOnly: true, deleted: false
+            }
+        ])
+    })
+}
+
+function addMember(meetingId, username) { 
+    return api.post(`/front/addUser/${meetingId}`, { username })
+    // .then((response) => Promise.resolve(response.data))
+    // .catch(handleErrors)
+    .catch(() => Promise.resolve('Return mockup Data'))
+    .then(()  => Promise.resolve({ message: "Membro adicionado!" }) )
+}
+
+function removeMember(meetingId, username) { 
+    return api.post(`/front/removeUser/${meetingId}`, { username })
+    // .then((response) => Promise.resolve(response.data))
+    // .catch(handleErrors)
+    .catch(() => Promise.resolve('Return mockup Data'))
+    .then(()  => Promise.resolve({ message: "Membro removido!" }) )
+}
