211ac1ffe4fd60380d70c18e0e9e93a4536fcd7f
Van Loon <gabriel.vl.rojas@gmail.com>
2021-03-11 12:04:24 -0300

    Update userService, remove otherCourse/Institution and disable mockup data

--- frontend/src/services/userService.js
+++ frontend/src/services/userService.js
@@ -15,14 +15,14 @@ function getById(userId) {
     return api.get('/user/' + userId)
     .then((response) => Promise.resolve(response.data))
     .catch(handleErrors)
-    .catch(() => Promise.resolve('Return mockup Data'))
-    .then(() => {
-        return Promise.resolve({
-            id: "123abc", name: 'Zézinho 013', course: 'BCC', otherCourse: '', institution: 'USP/ICMC', otherInstitution: '',
-            collegeID: '1337013', yearJoinCollege: "2021", yearJoinGanesh:  "2021",
-            email: 'ze@zinho.com', username: 'Z3z1nh013', title: 'Membro', role:  60,
-        })
-    })
+    // .catch(() => Promise.resolve('Return mockup Data'))
+    // .then(() => {
+    //     return Promise.resolve({
+    //         id: "123abc", name: 'Zézinho 013', course: 'BCC', institution: 'USP/ICMC',
+    //         collegeID: '1337013', yearJoinCollege: "2021", yearJoinGanesh:  "2021",
+    //         email: 'ze@zinho.com', username: 'Z3z1nh013', title: 'Membro', role:  60,
+    //     })
+    // })
 }
 
 function getAll() { 
