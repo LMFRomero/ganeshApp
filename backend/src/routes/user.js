b58a15b394a83b095b6552c775e58eecd8a39969
Lucas <lucasmfromero@gmail.com>
2021-03-15 18:40:38 -0300

    feat: use isCoordOrSelf middleware in user routes

--- backend/src/routes/user.js
+++ backend/src/routes/user.js
@@ -7,7 +7,6 @@ const UserController = require('../controllers/UserController');
 const ResetPasswordController = require('../controllers/ResetPasswordController');
 
 const permsMiddlewares = require('../middlewares/perms');
-const privilegeMan = require('../services/privilege');
 
 //route: /api/user
 
@@ -18,15 +17,9 @@ routes.post('/resetPassword/:token', ResetPasswordController.update);
 //auth routes
 routes.use(permsMiddlewares.isAuth);
 
-
-routes.put('/:id', (req, res, next) => {
-    if (permsMiddlewares.isCoordinator(req, res) || permsMiddlewares.isSelf(req, res)) {
-        next();
-    }
-    else {
-        return res.status(401).end();
-    }
-}, UserController.update);
+routes.get('/', permsMiddlewares.isCoordOrIsSelf, UserController.show);
+routes.get('/:id', permsMiddlewares.isCoordOrIsSelf, UserController.show);
+routes.put('/:id', permsMiddlewares.isCoordOrIsSelf, UserController.update);
 
 routes.post('/acceptUser', permsMiddlewares.isCoordinator, RequestUserController.update);
 routes.post('/rejectUser', permsMiddlewares.isCoordinator, RequestUserController.destroy);
