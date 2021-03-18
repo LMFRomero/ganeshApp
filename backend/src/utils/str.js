b03a4977e43740a15b1da6ae9a3392cb71b819e5
Lucas <lucasmfromero@gmail.com>
2021-03-17 21:09:22 -0300

    feat: create string treatment file

---
+++ backend/src/utils/str.js
@@ -0,0 +1,24 @@
+const Regex = require('regex');
+module.exports = {
+
+    validateString(str, fieldName, maxLen, regex) {
+        if (!str) {
+            return `O campo '${fieldName}' é obrigatório`;
+        }
+    
+        if (str.length > maxLen) {
+            return `O campo '${fieldName}' só aceita no máximo ${maxLen} caracteres`;
+        }
+    
+        if (regex) {
+            const reg = new Regex(regex);
+            if (reg.test(str) == false) {
+                return `O campo ${fieldName} contem caracteres inválidos`;
+            }
+        }
+    
+        else {
+            return null;
+        }
+    }
+}
