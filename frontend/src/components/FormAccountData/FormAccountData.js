76008a43e2d4ecf6116f280f3f823e2a24f59ef7
Van Loon <gabriel.vl.rojas@gmail.com>
2021-03-25 21:26:56 -0300

    update: FormAccountData parse role to Int

--- frontend/src/components/FormAccountData/FormAccountData.js
+++ frontend/src/components/FormAccountData/FormAccountData.js
@@ -11,7 +11,7 @@ function FormAccountData({ variant, submitDisabled, setSubmitDisabled, formData,
 
   const handleChange = (e) => {
     const {name, value} = e.target
-    setFormData({...formData, [name]: value})
+    setFormData({...formData, [name]: parseInt(value)})
   }
 
   return(
