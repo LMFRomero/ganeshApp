9700569207d5f5740b4a0f2a04b969887d89c864
Lucas <lucasmfromero@gmail.com>
2021-03-30 16:31:29 -0300

    feat: add get fronts endpoint

--- backend/src/routes/front.js
+++ backend/src/routes/front.js
@@ -8,6 +8,10 @@ const permsMiddlewares = require('../middlewares/perms');
 
 //route: /api/front
 
+routes.use(permsMiddlewares.isAuth);
+
+routes.get('/', FrontController.show);
+routes.get('/:id', FrontController.show);
 routes.post('/', permsMiddlewares.isCoordinator, FrontController.store);
 routes.put('/:frontName', permsMiddlewares.isCoordinator, FrontController.update);
 routes.delete('/:frontName', permsMiddlewares.isCoordinator, FrontController.destroy);
