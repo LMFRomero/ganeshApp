5bde68bc51d355abca8b6854e859f7bcf154e736
Van Loon <gabriel.vl.rojas@gmail.com>
2021-03-31 10:03:52 -0300

    Remove Old Frontend folder

--- old_frontend/src/pages/LoginRegister/pages/ForgotPasswordPage.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import AbstractLoginRegister, { ALRProps } from '../AbstractLoginRegister';
-
-export default function ForgotPasswordPage(props: ALRProps) {
-    return (
-        <AbstractLoginRegister {...props} formType="ForgotPassword"/>
-    )
-}
