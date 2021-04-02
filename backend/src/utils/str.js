8258952c775d76fe1b7eeafa61e5965b9886b574
Lucas <lucasmfromero@gmail.com>
2021-04-02 16:26:27 -0300

    feat: add new regex

--- backend/src/utils/str.js
+++ backend/src/utils/str.js
@@ -1,7 +1,10 @@
 const regexp = {
     slugName: /^[a-zA-Z0-9-]*$/g,
     alpha: /^[a-zA-Z ]*$/g,
+    num: /^[0-9]*$/g,
     alNum: /^[a-zA-Z0-9 ]*$/g,
+    email: /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/,
+    password: /^[a-zA-Z0-9@$!%*#?&]*$/g,
 };
 
 module.exports = {
