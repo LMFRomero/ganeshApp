f0d5485b1f792c691398596709eb19cb54019cd0
Lucas <lucasmfromero@gmail.com>
2021-04-09 18:05:25 -0300

    fix: member can delete meeting

--- backend/src/routes/meeting.js
+++ backend/src/routes/meeting.js
@@ -3,6 +3,8 @@ const routes = express.Router();
 
 
 const MeetingController = require('../controllers/MeetingController');
+const FrequencyCodeController = require('../controllers/FrequencyCodeController');
+
 
 const permsMiddlewares = require('../middlewares/perms');
 
@@ -15,8 +17,8 @@ routes.get('/:id', MeetingController.show);
 routes.get('/', MeetingController.show);
 routes.post('/', MeetingController.store);
 routes.put('/:id', MeetingController.update);
-routes.delete('/:id', permsMiddlewares.isCoordinator, MeetingController.destroy);
-routes.post('/generate/:id', MeetingController.generateMeetingCode);
+routes.delete('/:id', permsMiddlewares.isMember, MeetingController.destroy);
+routes.post('/generate/:id', FrequencyCodeController.store);
 routes.post('/:code', MeetingController.checkTime, MeetingController.checkMemberFrequency);
 
 module.exports = routes;
