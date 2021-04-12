877ae41f8bebb0c177d2ac896cf507cae99f37c2
Lucas <lucasmfromero@gmail.com>
2021-04-12 11:43:05 -0300

    feat: FrequencyCode model

---
+++ backend/src/models/FrequencyCode.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose');
+const ttl = require('mongoose-ttl');
+
+const frequencyCode = new mongoose.Schema ({
+    code: {
+        type: Number,
+        required: true,
+        unique: true,
+    },
+    meeting: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Meeting',
+        required: true,
+    },
+    creator: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true,
+    },
+
+    createdAt: {
+        type: Date,
+        required: true,
+    }
+});
+
+frequencyCode.plugin(ttl);
+
+module.exports = mongoose.model('frequencyCode', frequencyCode);
